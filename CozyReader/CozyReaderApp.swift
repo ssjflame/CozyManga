ac9433b18e2386597553d20b4416da60ae34d5c1
{ssjflame} <{albertsufhsu@gmail.com}>
2026-06-22 00:51:16 +0100

    Initial Commit

---
+++ CozyReader/CozyReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CozyReaderApp.swift
+//  CozyReader
+//
+//  Created by Albert Barbu on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CozyReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
